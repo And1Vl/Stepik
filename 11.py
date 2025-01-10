9ddd611cbdbbdc4054621bab6af7c5635f6279a9
Vladimir <kazemirov.v.a@yandex.ru>
2025-01-10 21:45:40 +0300

    Add files via upload

--- 11.py
+++ 11.py
@@ -167,3 +167,182 @@ for i in range(n):
     a = input()
     my_list.extend(a)
 print(my_list)
+
+
+
+numbers = [1, 78, 23, -65, 99, 9089, 34, -32, 0, -67, 1, 11, 111]
+a = [i**2 for i in numbers]
+print(sum(a))
+
+
+
+xs = []
+qs = []
+for i in range(1, int(input()) + 1):
+    x = int(input())
+    xs.append(x)
+    q = x * x + 2 * x + 1
+    qs.append(q)
+print(* xs, sep = '\n')
+print()
+print(* qs, sep = '\n')
+
+
+
+my_list = []
+for i in range(int(input())):
+    n = int(input())
+    my_list.append(n)
+for i in my_list:
+    if i != min(my_list) and i != max(my_list):
+        print(i)
+
+
+
+my_list = []
+for i in range(int(input())):
+    n = input()
+    if n not in my_list:
+        my_list.append(n)
+print(* my_list, sep = '\n')
+
+
+
+my_list = []
+for i in range(int(input())):
+    n = input()
+    my_list.append(n)
+s = input()
+for i in my_list:
+    if s.lower() in i.lower():
+        print(i)
+
+
+
+my_list = []
+my_list2 = []
+for i in range(int(input())):
+    my_list.append(input())
+for i in range(int(input())):
+    my_list2.append(input())
+for i in my_list:
+    count = 0
+    for j in my_list2:
+        if j.lower() in i.lower():
+            count += 1
+    if count >= len(my_list2):
+        print(i)
+
+
+
+neg = []
+zer = []
+pos = []
+for i in range(int(input())):
+    n = int(input())
+    if n < 0:
+        neg.append(n)
+    elif n == 0:
+        zer.append(n)
+    else:
+        pos.append(n)
+res = neg + zer + pos
+print(* res, sep = '\n')
+
+
+
+s = input()
+words = s.split()
+print(* words, sep = '\n')
+
+
+
+s = input().split()
+for i in s:
+    print(i[0], end = '.')
+
+
+
+s = input().split('\\')
+print(* s, sep = '\n')
+
+
+
+s = input().split()
+for i in s:
+    print('+' * int(i))
+
+
+
+s = input().split('.')
+for i in s:
+    if int(i) < 0 or int(i) > 255:
+        print('НЕТ')
+        break
+else:
+    print('ДА')
+
+
+
+s = input()
+n = input()
+result = n.join(list(s))
+print(result)
+
+
+
+s = input().split()
+count = 0
+for i in range(len(s)):
+    for j in range(i+1,len(s)):
+        if s[i] == s[j]:
+            count += 1
+print(count)
+
+
+
+numbers = [8, 9, 10, 11]
+numbers[1] = 17
+numbers.extend([4, 5, 6])
+del numbers[0]
+numbers *= 2
+numbers.insert(3,25)
+print(numbers)
+
+
+
+l = []
+s = input().split()
+for i in s:
+    l.append(int(i))
+min_i = l.index(min(l))
+max_i = l.index(max(l))
+l[max_i], l[min_i] = l[min_i], l[max_i]
+print(*l)
+
+
+
+s = input().lower().split()
+print(f"Общее количество артиклей: {s.count('a') + s.count('an') + s.count('the')}")
+
+
+
+s = input()
+for i in range(int(s[1:])):
+    a = input()
+    if'#' in a:
+        a = a[:a.find('#')]
+    print(a.rstrip())
+
+
+
+n = input().split()
+for i in range(len(n)):
+    n[i] = int(n[i])
+n.sort()
+print(*n)
+n.reverse()
+print(*n)
+
+
+
